7e0f132181fd04dec5b0d12e5242a35545d0dbb3
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-17 01:11:21 +0530

    create routes for user-products, admin-products & add to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,18 @@ app.get("/",(req,res)=>{
     return res.status(200).send({message:"welcome to Clothing store - server", status:true})
 });
 
+// User
 const authRouter = require("./routes/auth.routes.js");
 app.use("/auth",authRouter);
 
 const userRouter = require("./routes/user.routes.js");
 app.use("/api/users",userRouter);
+
+// Products
+const productRouter = require("./routes/product.routes.js");
+app.use("/api/products",productRouter);
+
+const adminProductRouter = require("./routes/product.admin.routes.js");
+app.use("/api/admin/products",adminProductRouter);
 
 module.exports={app};
