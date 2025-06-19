588a28beea01b4244106ea386dd03694012281f8
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-19 23:05:22 +0530

    ActionTypes & actions written for findProductsByCateg. & findProductById

---
+++ frontend/src/redux/customer/product/Action.js
@@ -0,0 +1,55 @@
+import { FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS } from "./ActionTypes";
+
+// find products by category - action
+export const findProducts = (reqData) => async (dispatch) => {
+  dispatch({ type: FIND_PRODUCTS_REQUEST });
+
+  const { colors, sizes, minPrice, maxPrice, minDiscount, category, stock, sort, pageNumber, pageSize, } = reqData;
+
+  try {
+    
+    const { data } = await api.get(
+      `/api/products?color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
+    );
+
+    console.log("get product by category: ", data);
+
+    dispatch({
+      type: FIND_PRODUCTS_SUCCESS,
+      payload: data,
+    });
+
+  } catch (error) {
+    dispatch({
+      type: FIND_PRODUCTS_FAILURE,
+      payload: error.message,
+    });
+  }
+}
+
+// find products by Id - action
+export const findProductsById = (reqData) => async (dispatch) => {
+  dispatch({ type: FIND_PRODUCT_BY_ID_REQUEST });
+
+  const { productId } = reqData;
+
+  try {
+    
+    const { data } = await api.get(
+      `/api/products/id/${productId}`
+    );
+
+    console.log("get product by id: ", data);
+
+    dispatch({
+      type: FIND_PRODUCT_BY_ID_SUCCESS,
+      payload: data,
+    });
+
+  } catch (error) {
+    dispatch({
+      type: FIND_PRODUCT_BY_ID_FAILURE,
+      payload: error.message,
+    });
+  }
+}
