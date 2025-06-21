3190b34859f571d8d938a2821374c94c519576a4
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-21 20:36:23 +0530

    actionType, Action, Reducer: for create,update,delete Products

--- frontend/src/redux/customer/product/Action.js
+++ frontend/src/redux/customer/product/Action.js
@@ -1,5 +1,5 @@
-import { api } from "../../../config/apiConfig";
-import { FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS } from "./ActionTypes";
+import { api, API_BASE_URL } from "../../../config/apiConfig";
+import { CREATE_PRODUCT_FAILURE, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, DELETE_PRODUCT_FAILURE, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS, UPDATE_PRODUCT_FAILURE, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_SUCCESS } from "./ActionTypes";
 
 // find products by category - action
 export const findProducts = (reqData) => async (dispatch) => {
@@ -55,3 +55,84 @@ export const findProductsById = (reqData) => async (dispatch) => {
     });
   }
 }
+
+// ADMIN
+// create product
+export const createProduct = (product) => async (dispatch) => {
+  try {
+    dispatch({ type: CREATE_PRODUCT_REQUEST });
+
+    const { data } = await api.post(
+      `${API_BASE_URL}/api/admin/products/`,
+      product.data
+    );
+
+    dispatch({
+      type: CREATE_PRODUCT_SUCCESS,
+      payload: data,
+    });
+
+    console.log("created product ", data);
+  } catch (error) {
+    dispatch({
+      type: CREATE_PRODUCT_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// update product
+export const updateProduct = (product) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PRODUCT_REQUEST });
+
+    const { data } = await api.put(
+      `${API_BASE_URL}/api/admin/products/${product.productId}`,
+      product
+    );
+console.log("update product ",data)
+    dispatch({
+      type: UPDATE_PRODUCT_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PRODUCT_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// delete product
+export const deleteProduct = (productId) => async (dispatch) => {
+  console.log("delete product action",productId)
+  try {
+    dispatch({ type: DELETE_PRODUCT_REQUEST });
+
+    let {data}=await api.delete(`/api/admin/products/${productId}`);
+
+    console.log("delete product ",data)
+
+    dispatch({
+      type: DELETE_PRODUCT_SUCCESS,
+      payload: productId,
+    });
+
+    console.log("product delte ",data)
+  } catch (error) {
+    console.log("catch error ",error)
+    dispatch({
+      type: DELETE_PRODUCT_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/redux/customer/product/Reducer.js
+++ frontend/src/redux/customer/product/Reducer.js
@@ -1,4 +1,4 @@
-import { FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS } from "./ActionTypes";
+import { CREATE_PRODUCT_FAILURE, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, DELETE_PRODUCT_FAILURE, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS, UPDATE_PRODUCT_FAILURE, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_SUCCESS } from "./ActionTypes";
 
 const initialState = {
   products: [],
@@ -23,6 +23,71 @@ export const customerProductReducer = (state = initialState, action) => {
     case FIND_PRODUCTS_FAILURE:
     case FIND_PRODUCT_BY_ID_FAILURE:
       return { ...state, loading: false, error: action.payload };
+
+
+      // Admin
+    case CREATE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+    case CREATE_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          products: [...state.products, action.payload],
+        };
+    case CREATE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+
+
+    case UPDATE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+    case UPDATE_PRODUCT_SUCCESS:
+      
+        return {
+          ...state,
+          loading: false,
+          products: state.products.map((product) =>
+            product.id === action.payload.id ? action.payload : product
+          ),
+        };
+    case UPDATE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+
+    
+    case DELETE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+    case DELETE_PRODUCT_SUCCESS:
+        console.log("dlete ",state.products)
+        return {
+          ...state,
+          loading: false,
+          deleteProduct:action.payload
+        };
+    case DELETE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
 
     default:
       return state;
