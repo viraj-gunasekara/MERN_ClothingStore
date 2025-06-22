6731ef9ded5aad0ba169246ed7ad0c01c26187cd
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-22 19:59:02 +0530

    update product model, initialize to open: onclick

--- frontend/src/admin/componets/UpdateProductForm.jsx
+++ frontend/src/admin/componets/UpdateProductForm.jsx
@@ -1,12 +1,34 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box, Card, Modal } from "@mui/material";
+import React from "react";
 
-const UpdateProductForm = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  outline: "none",
+};
+
+const UpdateProductForm = ({ handleClose, open }) => {
   return (
-    <Box sx={{ padding: 2, paddingLeft: 25 }}>
-    <div>UpdateProductForm</div>
-    </Box>
-  )
-}
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      size="large"
+    >
+        <Box sx={{ padding: 2, paddingLeft: 25, style }} className="rounded-md">
+          <Card className="mt-2 p-6">
+              Hi
+          </Card>
+        </Box>
+    </Modal>
+  );
+};
 
-export default UpdateProductForm
+export default UpdateProductForm;
